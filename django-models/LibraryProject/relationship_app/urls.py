273e814d866433beefc860733e83c02eff0f0d2a
Charwekey <crabisabutey@gmail.com>
2025-11-09 17:34:03 +0000

    adde more html files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,3 +6,12 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+]
