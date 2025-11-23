101fcc93d464fa3e8903185f68ba698c953c162c
Charwekey <crabisabutey@gmail.com>
2025-11-23 17:23:11 +0000

    Added advancedfeatures

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,18 @@
 
 from django.db import models
+from django.conf import settings
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    added_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='added_books'
+    )
 
     class Meta:
         permissions = (
