de0f5af45b9c0bd529882a82d791cc4a4008a8c1
Charwekey <crabisabutey@gmail.com>
2025-12-07 12:22:32 +0000

    Blog Post Management Features

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -5,6 +5,16 @@ from django.contrib.auth.models import User
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField()
 
+from .models import Post
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField()
+
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
